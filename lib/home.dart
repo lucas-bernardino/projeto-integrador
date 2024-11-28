9e0b4754f06684c4490aeaa4d35801253492e591
Lucas Bernardino <lucasgber@gmail.com>
2024-11-28 15:30:26 -0300

    Start changing background of home page

--- lib/home.dart
+++ lib/home.dart
@@ -19,52 +19,64 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[900],
-      body: Column(
-        children: [
-          SizedBox(height: 50,),
-          TextButton(
-              onPressed: () {
-                setState(() {
-                  isDebug = !isDebug;
-                });
-              },
-              child: Text("Modo Debug")
-          ),
-          SizedBox(height: 200,),
-          Column(
-            children: [
-              Text("MATERIAL DOURADO", style: TextStyle(color: Colors.yellow[800], fontSize: 50, fontWeight: FontWeight.w900),),
-              SizedBox(height: 100,),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    style: ButtonStyle(backgroundColor: WidgetStatePropertyAll(Colors.yellowAccent)),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => Game()),
-                        );
-                      },
-                      child: Text("Jogar", style: TextStyle(color: Colors.black),)
+      body: Container(
+        decoration: BoxDecoration(
+            color: Colors.red,
+            image: DecorationImage(
+              fit: BoxFit.cover,//<--- add this
+              image: AssetImage('assets/background.jpg'),
+            )),
+        child: Column(
+          children: [
+            SizedBox(height: 50,),
+            TextButton(
+                onPressed: () {
+                  setState(() {
+                    isDebug = !isDebug;
+                  });
+                },
+                child: Text("Modo Debug")
+            ),
+            SizedBox(height: 200,),
+            Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(30))
                   ),
-                  SizedBox(width: 30,),
-                  ElevatedButton(
+                    child: Text("MATERIAL DOURADO", style: TextStyle(color: Colors.yellow[400], fontSize: 50, fontWeight: FontWeight.w900, backgroundColor: Colors.yellow[900]),)),
+                SizedBox(height: 100,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
                       style: ButtonStyle(backgroundColor: WidgetStatePropertyAll(Colors.yellowAccent)),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => Tutorial()),
-                        );
-                      },
-                      child: Text("Tutorial", style: TextStyle(color: Colors.black),)
-                  ),
-                  isDebug ? Serial() : SizedBox(),
-                ],
-              ),
-            ],
-          ),
-        ],
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => Game()),
+                          );
+                        },
+                        child: Text("Jogar", style: TextStyle(color: Colors.black, fontSize: 20),)
+                    ),
+                    SizedBox(width: 30,),
+                    ElevatedButton(
+                        style: ButtonStyle(backgroundColor: WidgetStatePropertyAll(Colors.yellowAccent)),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => Tutorial()),
+                          );
+                        },
+                        child: Text("Tutorial", style: TextStyle(color: Colors.black, fontSize: 20),)
+                    ),
+                    isDebug ? Serial() : SizedBox(),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
 
